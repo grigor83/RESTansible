001ee60be2b5d3cf1ba5fcf1e373cef730eeff32
grigor <grigor@mail.com>
2025-04-15 01:14:09 +0200

    update ddl script

--- database_DLL.sql
+++ database_DLL.sql
@@ -8,7 +8,7 @@ CREATE TABLE user
 (
 	id					int	 			not null auto_increment,
     username			varchar(50) 	not null,
-    password			varchar(50) 	not null,
+    password			varchar(500) 	not null,
     phone				varchar(50) 	not null,
     email				varchar(50) 	not null,
     PRIMARY KEY(id)
@@ -19,7 +19,7 @@ CREATE TABLE inventory
 (
 	id					int	 			not null auto_increment,
 	filename 			varchar(100)	not null, 
-    filepath 			varchar(500)	not null, 
+    filepath 			varchar(500), 
     user_id				int,
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES user(id) on delete cascade
@@ -36,20 +36,10 @@ CREATE TABLE playbook
     FOREIGN KEY(user_id) REFERENCES user(id) on delete cascade
 );
 
-
-DROP TABLE IF EXISTS administrator;
-CREATE TABLE administrator 
-(
-	id					int	 			not null auto_increment,
-    username			varchar(50)    not null,
-    password			varchar(50)	not null,
-    PRIMARY KEY(id)
-);
-
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=1 */;
 
-insert into administrator(username, password) values ('admin','admin');
-insert into inventory(filename, filepath) values ('inventory', 'nesto');
+insert into user(username, password, phone, email) values ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '', '');
+insert into inventory(filename) values ('hosts');
 insert into playbook(filename) values ('create_loopback0.yaml'), ('create_loopbacks.yaml'), ('delete_loopback0.yaml'), 
 ('interfaces_facts.yaml'), ('ios_facts.yaml'), ('list_interfaces.yaml');
 
