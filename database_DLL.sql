98ee8d90713ad03b7b05f007342b95116ff5f0b0
grigor <grigor@mail.com>
2025-04-16 23:49:23 +0200

    added new playbook show_version

--- database_DLL.sql
+++ database_DLL.sql
@@ -41,7 +41,7 @@ CREATE TABLE playbook
 insert into user(username, password, phone, email) values ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '', '');
 insert into inventory(filename) values ('hosts');
 insert into playbook(filename) values ('create_loopback0.yaml'), ('create_loopbacks.yaml'), ('delete_loopback0.yaml'), 
-('interfaces_facts.yaml'), ('ios_facts.yaml'), ('list_interfaces.yaml');
+('interfaces_facts.yaml'), ('ios_facts.yaml'), ('list_interfaces.yaml'), ('show_version.yaml');
 
 
 
